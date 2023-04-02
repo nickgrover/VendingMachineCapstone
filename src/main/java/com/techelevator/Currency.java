6bba7e2e43c27492f8e3085882bf7b01be1dbbc6
Brian Garza <bcgarza0823@gmaillcom[B[D[D[.[C[C[C[C>
2023-04-02 00:02:35 -0400

    capstone push

--- src/main/java/com/techelevator/Currency.java
+++ src/main/java/com/techelevator/Currency.java
@@ -1,6 +1,7 @@
 package com.techelevator;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class Currency {
 
@@ -8,9 +9,9 @@ public class Currency {
 //    private final double DIME = .10;
 //    private final double QUARTER = .25;
 
-    private final BigDecimal NICKEL = new BigDecimal(.05);
-    private final BigDecimal DIME = new BigDecimal(.10);
-    private final BigDecimal QUARTER = new BigDecimal(.25);
+    private final BigDecimal NICKEL = new BigDecimal(".05");
+    private final BigDecimal DIME = new BigDecimal(".10");
+    private final BigDecimal QUARTER = new BigDecimal(".25");
 
 
     public Currency() {
@@ -34,17 +35,33 @@ public class Currency {
         int numberOfQuarters = 0;
         int numberOfNickels = 0;
         int numberOfDimes = 0;
+        BigDecimal tracker = currentMoneyProvided;
+
+
+
+        while(tracker.compareTo(QUARTER) >= 0) {
+            tracker = tracker.subtract(QUARTER);
+            numberOfQuarters++;
+        }while (tracker.compareTo(DIME)>=0){
+            tracker = (tracker.subtract(DIME));
+            numberOfDimes++;
+        }while (tracker.compareTo(NICKEL)>=0){
+            tracker = (tracker.subtract(NICKEL));
+            numberOfNickels++;
+        }
+
+
 //        double remainder;
-        BigDecimal divideByQuarter = currentMoneyProvided.divideToIntegralValue(QUARTER);
-        currentMoneyProvided = currentMoneyProvided.subtract(divideByQuarter.multiply(QUARTER));
-        BigDecimal divideByDime = currentMoneyProvided.divideToIntegralValue(DIME);
-        currentMoneyProvided = currentMoneyProvided.subtract(divideByDime.multiply(DIME));
-        BigDecimal divideByNickel = currentMoneyProvided.divideToIntegralValue(NICKEL);
-        currentMoneyProvided = currentMoneyProvided.subtract(divideByNickel.multiply(NICKEL));
+//        BigDecimal divideByQuarter = currentMoneyProvided.divide(QUARTER);
+//        currentMoneyProvided = currentMoneyProvided.subtract(divideByQuarter.multiply(QUARTER));
+//        BigDecimal divideByDime = currentMoneyProvided.divide(DIME);
+//        currentMoneyProvided = currentMoneyProvided.subtract(divideByDime.multiply(DIME));
+//        BigDecimal divideByNickel = currentMoneyProvided.divide(NICKEL);
+//        currentMoneyProvided = currentMoneyProvided.subtract(divideByNickel.multiply(NICKEL));
         System.out.println("Returned change: ");
-        System.out.println("Number of quarters: " + divideByQuarter);
-        System.out.println("Number of dimes: " + divideByDime);
-        System.out.println("Number of nickels: " + divideByNickel);
+        System.out.println("Number of quarters: " + numberOfQuarters);
+        System.out.println("Number of dimes: " + numberOfDimes);
+        System.out.println("Number of nickels: " + numberOfNickels);
         }
 
         }
